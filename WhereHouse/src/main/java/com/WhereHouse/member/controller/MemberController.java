f379a3eaadc37bcd3a32b086a522e5141ef7f0f9
admin <admin@192.168.0.106>
2016-11-21 18:07:01 +0900

    메인

--- WhereHouse/src/main/java/com/WhereHouse/member/controller/MemberController.java
+++ WhereHouse/src/main/java/com/WhereHouse/member/controller/MemberController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/WH")
 public class MemberController {
-	@RequestMapping("/main")
-	public void main() {
-
+	
+	@RequestMapping("/")
+	public String main() {
+		return "main";
 	}
 }
--- WhereHouse/src/main/java/com/WhereHouse/www/HomeController.java
+++ WhereHouse/src/main/java/com/WhereHouse/www/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -36,10 +36,7 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping("/index")
-	public void test(){
-		
-	}
+
 	
 	
 	
