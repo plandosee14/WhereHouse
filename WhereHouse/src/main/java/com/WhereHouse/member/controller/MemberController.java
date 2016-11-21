9d82752e976192a371b0de31cb038d9217e4fecd
admin <admin@192.168.0.106>
2016-11-21 17:51:30 +0900

    123

---
+++ WhereHouse/src/main/java/com/WhereHouse/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.WhereHouse.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/WH")
+public class MemberController {
+	@RequestMapping("/main")
+	public void main() {
+
+	}
+}
