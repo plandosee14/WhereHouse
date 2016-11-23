ca8c7a5081e425784c10b1fce2ebdac8d9f42cf6
june <june@192.168.0.109>
2016-11-23 16:58:19 +0900

    DDD

--- WhereHouse/src/main/java/com/WhereHouse/comment/persistence/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author À±¿µ¹Ì
- *
- */
-package com.WhereHouse.comment.persistence;
--- WhereHouse/src/main/resources/sql/comment.sql
+++ WhereHouse/src/main/resources/sql/comment.sql
@@ -3,6 +3,7 @@ create table comment(
 	c_no number		primary key,
 	m_no varchar2(50)	references member(m_no),
 	m_name varchar2(50)	references member(m_name),
+	h_no varchar2(50)	references house(h_no),
 	c_content varchar(1500) not null,
 	c_regdate date		default sysdate,
 	c_modifydate date	default sysdate
