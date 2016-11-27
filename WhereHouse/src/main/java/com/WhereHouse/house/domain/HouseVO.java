e37f6bb36570f7923f4110a36393c5dcdd72c726
윤영미 <@192.168.0.17>
2016-11-27 16:27:03 +0900

    HouseVO

--- WhereHouse/src/main/java/com/WhereHouse/house/domain/HouseVO.java
+++ WhereHouse/src/main/java/com/WhereHouse/house/domain/HouseVO.java
@@ -1,5 +1,173 @@
 package com.WhereHouse.house.domain;
 
+import java.util.Date;
+
 public class HouseVO {
+	private int h_no;  	      
+	private String m_id; 
+	private String m_name; 							
+	private String h_addres;	        
+	private int h_fare;
+	private Date h_startdate;
+	private Date h_enddate;	
+	private String h_info;		
+	private int h_peoplecnt;
+	private String h_type;
+	private String h_checktime;
+	private int h_sale;
+	private int h_salefare;
+	private String h_rule;
+	private String h_thumnail;
+	private int h_state;
+	private int h_scorecnt;
+	private int h_avgscore;
+	private int h_rightscore;
+	private int h_cleanscore;
+	private int h_checkscore;
+	private int h_commuscore;
+	public int getH_no() {
+		return h_no;
+	}
+	public void setH_no(int h_no) {
+		this.h_no = h_no;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getH_addres() {
+		return h_addres;
+	}
+	public void setH_addres(String h_addres) {
+		this.h_addres = h_addres;
+	}
+	public int getH_fare() {
+		return h_fare;
+	}
+	public void setH_fare(int h_fare) {
+		this.h_fare = h_fare;
+	}
+	public Date getH_startdate() {
+		return h_startdate;
+	}
+	public void setH_startdate(Date h_startdate) {
+		this.h_startdate = h_startdate;
+	}
+	public Date getH_enddate() {
+		return h_enddate;
+	}
+	public void setH_enddate(Date h_enddate) {
+		this.h_enddate = h_enddate;
+	}
+	public String getH_info() {
+		return h_info;
+	}
+	public void setH_info(String h_info) {
+		this.h_info = h_info;
+	}
+	public int getH_peoplecnt() {
+		return h_peoplecnt;
+	}
+	public void setH_peoplecnt(int h_peoplecnt) {
+		this.h_peoplecnt = h_peoplecnt;
+	}
+	public String getH_type() {
+		return h_type;
+	}
+	public void setH_type(String h_type) {
+		this.h_type = h_type;
+	}
+	public String getH_checktime() {
+		return h_checktime;
+	}
+	public void setH_checktime(String h_checktime) {
+		this.h_checktime = h_checktime;
+	}
+	public int getH_sale() {
+		return h_sale;
+	}
+	public void setH_sale(int h_sale) {
+		this.h_sale = h_sale;
+	}
+	public int getH_salefare() {
+		return h_salefare;
+	}
+	public void setH_salefare(int h_salefare) {
+		this.h_salefare = h_salefare;
+	}
+	public String getH_rule() {
+		return h_rule;
+	}
+	public void setH_rule(String h_rule) {
+		this.h_rule = h_rule;
+	}
+	public String getH_thumnail() {
+		return h_thumnail;
+	}
+	public void setH_thumnail(String h_thumnail) {
+		this.h_thumnail = h_thumnail;
+	}
+	public int getH_state() {
+		return h_state;
+	}
+	public void setH_state(int h_state) {
+		this.h_state = h_state;
+	}
+	public int getH_scorecnt() {
+		return h_scorecnt;
+	}
+	public void setH_scorecnt(int h_scorecnt) {
+		this.h_scorecnt = h_scorecnt;
+	}
+	public int getH_avgscore() {
+		return h_avgscore;
+	}
+	public void setH_avgscore(int h_avgscore) {
+		this.h_avgscore = h_avgscore;
+	}
+	public int getH_rightscore() {
+		return h_rightscore;
+	}
+	public void setH_rightscore(int h_rightscore) {
+		this.h_rightscore = h_rightscore;
+	}
+	public int getH_cleanscore() {
+		return h_cleanscore;
+	}
+	public void setH_cleanscore(int h_cleanscore) {
+		this.h_cleanscore = h_cleanscore;
+	}
+	public int getH_checkscore() {
+		return h_checkscore;
+	}
+	public void setH_checkscore(int h_checkscore) {
+		this.h_checkscore = h_checkscore;
+	}
+	public int getH_commuscore() {
+		return h_commuscore;
+	}
+	public void setH_commuscore(int h_commuscore) {
+		this.h_commuscore = h_commuscore;
+	}
+	@Override
+	public String toString() {
+		return "HouseVO [h_no=" + h_no + ", m_id=" + m_id + ", m_name=" + m_name + ", h_addres=" + h_addres
+				+ ", h_fare=" + h_fare + ", h_startdate=" + h_startdate + ", h_enddate=" + h_enddate + ", h_info="
+				+ h_info + ", h_peoplecnt=" + h_peoplecnt + ", h_type=" + h_type + ", h_checktime=" + h_checktime
+				+ ", h_sale=" + h_sale + ", h_salefare=" + h_salefare + ", h_rule=" + h_rule + ", h_thumnail="
+				+ h_thumnail + ", h_state=" + h_state + ", h_scorecnt=" + h_scorecnt + ", h_avgscore=" + h_avgscore
+				+ ", h_rightscore=" + h_rightscore + ", h_cleanscore=" + h_cleanscore + ", h_checkscore=" + h_checkscore
+				+ ", h_commuscore=" + h_commuscore + "]";
+	}
+	
+	
 
 }
