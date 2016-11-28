390438808a3e6eda79728c9af4b29a7adb9fca6a
윤영미 <@192.168.0.121>
2016-11-28 15:54:06 +0900

    test중

--- WhereHouse/src/main/java/com/WhereHouse/house/controller/HouseRegisterController.java
+++ WhereHouse/src/main/java/com/WhereHouse/house/controller/HouseRegisterController.java
@@ -3,11 +3,16 @@ package com.WhereHouse.house.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/house")
 @Controller
 public class HouseRegisterController {
 
 	@RequestMapping("registerForm")
 	public void Register(){
+		
+	}
+	@RequestMapping("test")
+	public void test(){
 		
 	}
 }
