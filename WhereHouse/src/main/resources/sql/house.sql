6b9c57694b4db2ef76b0681433f9b0120ca4c15e
junghoon <junghoon@192.168.0.118>
2016-11-28 17:28:22 +0900

    엥에에에에엥에에에에에에에베베베벱

--- WhereHouse/src/main/resources/sql/house.sql
+++ WhereHouse/src/main/resources/sql/house.sql
@@ -14,7 +14,7 @@ create table house(
 	h_livetype     varchar2(1000)		not null, --숙소유형 (집전체, 거실, 방)   
 	h_checktime  varchar2(1000)			not null, --체크인 체크아웃시간
 	h_sale           number     		    ,  --할인률
-	h_salefare      number(2,2)     	,         --할인된 가격
+	h_salefare      number    	,         --할인된 가격
 	h_rule     varchar2(3000)	 	not null, --규칙
 	h_thumnail varchar2(3000)		not null,
 	h_state        number		              default 0,
@@ -63,7 +63,37 @@ create table house_option(
 
 
  alter table house add(h_zip varchar2(20) not null);
- 
+ alter table house add(h_livetype  varchar2(1000)  not null);
+ alter table house add(h_salefare  varchar2(1000)  not null);
+  alter table house modify(h_salefare number not null);
+  
+  
  insert into tbl_user (id, upw, uname) values ('soonsir','1234','순실');
- insert into house (h_no,m_id,m_name, h_zip,h_address, h_fare ,h_startdate,h_enddate, h_info, h_peoplecnt, h_type,h_checktime, h_sale,h_salefare, h_rule ,h_thumnail)
- values (house_seq.nextval,'powerfeel@naver.com','최필한','12345','서울시 양천구 목5동',50000, '2016-11-30','2012-12-30','정훈이네 집으로 찾아오세요',5, );
+
+ insert into house 
+(h_no,m_id,m_name,
+ h_zip,h_address, h_fare,
+h_startdate,h_enddate, h_info,
+ h_peoplecnt, h_type,h_livetype,
+h_checktime, h_sale,h_salefare,
+ h_rule,h_thumnail)
+ values (house_seq.nextval,'powerfeel123@naver.com','최필한',
+'12345','서울시 양천구 목5동',50000,
+'2016-11-30','2017-12-30','정훈이네 집으로 찾아오세요',
+5,'아파트','방하나',
+'3시~11시', 10,45000,
+'담배피지마세요','test_thumnail1.jpg');
+
+ insert into house 
+(h_no,m_id,m_name,
+ h_zip,h_address, h_fare,
+h_startdate,h_enddate, h_info,
+ h_peoplecnt, h_type,h_livetype,
+h_checktime, h_sale,h_salefare,
+ h_rule,h_thumnail)
+ values (house_seq.nextval,'ojh5797@naver.com','오정훈',
+'12345','서울시 양천구 목5동',50000,
+'2016-11-30','2017-12-30','하은이네 집으로 찾아오세요',
+5,'아파트','방하나',
+'3시~11시', 10,45000,
+'담배피지마세요','test_thumnail1.jpg');
