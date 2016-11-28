b209272760c894404edbada97eb06f08559ac410
윤영미 <@192.168.0.121>
2016-11-28 16:51:21 +0900

    ㅌㅊㅇㄻ

--- WhereHouse/src/main/resources/sql/house.sql
+++ WhereHouse/src/main/resources/sql/house.sql
@@ -9,12 +9,13 @@ create table house(
 	h_startdate 	      date		    	not null,		--
 	h_enddate   	      date		    	not null,
 	h_info      varchar2(3000)		  	not null,
-	h_peoplecnt              number		     	not null,
-	h_type         varchar2(1000)		not null,
-	h_checktime  varchar2(1000)			not null,
-	h_sale           number     		    , 
+	h_peoplecnt              number		     	not null, --최대숙박인원
+	h_type         varchar2(1000)		not null, --집유형(아파트, 전원주택, )
+	h_livetype     varchar2(1000)		not null, --숙소유형 (집전체, 거실, 방)   
+	h_checktime  varchar2(1000)			not null, --체크인 체크아웃시간
+	h_sale           number     		    ,  --할인률
 	h_salefare      number(2,2)     	,         --할인된 가격
-	h_rule     varchar2(3000)	 	not null,
+	h_rule     varchar2(3000)	 	not null, --규칙
 	h_thumnail varchar2(3000)		not null,
 	h_state        number		              default 0,
 	h_scorecnt      number		         	default 0,
@@ -62,3 +63,7 @@ create table house_option(
 
 
  alter table house add(h_zip varchar2(20) not null);
+ 
+ insert into tbl_user (id, upw, uname) values ('soonsir','1234','순실');
+ insert into house (h_no,m_id,m_name, h_zip,h_address, h_fare ,h_startdate,h_enddate, h_info, h_peoplecnt, h_type,h_checktime, h_sale,h_salefare, h_rule ,h_thumnail)
+ values (house_seq.nextval,'powerfeel@naver.com','최필한','12345','서울시 양천구 목5동',50000, '2016-11-30','2012-12-30','정훈이네 집으로 찾아오세요',5, );
