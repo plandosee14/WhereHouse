629c26a692a20d18a217e26973b512c8c24ada64
윤영미 <@172.20.10.3>
2016-11-29 17:34:44 +0900

    house 컬럼 추가

--- WhereHouse/src/main/resources/sql/basket.sql
+++ WhereHouse/src/main/resources/sql/basket.sql
@@ -16,9 +16,11 @@ nocache;
 select (b_no, h_no, m_id) from basket;
 
 
-insert into basket(b_no, h_no, m_id) values(basket_seq.nextval, 19, 'powerfeel123@naver.com');
-insert into basket(b_no, h_no, m_id) values(basket_seq.nextval, 20, 'ojh5797@naver.com');
+insert into basket(b_no, h_no, m_id) values(basket_seq.nextval, 23, 'powerfeel123@naver.com');
+insert into basket(b_no, h_no, m_id) values(basket_seq.nextval, 22, 'ojh5797@naver.com');
+insert into basket(b_no, h_no, m_id) values(basket_seq.nextval, 24, 'ojh5797@naver.com');
 
+delete from basket;
 		select b_no, h_no, m_id
 		from basket
 		where m_id='ojh5797@naver.com'
