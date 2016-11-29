3bbed0e064dcc71ab792b1a02327629e342f3ad0
junghoon <junghoon@192.168.0.119>
2016-11-29 12:05:52 +0900

    장바구니 장바구니

--- WhereHouse/src/main/java/com/WhereHouse/basket/persistence/BasketDAO.java
+++ WhereHouse/src/main/java/com/WhereHouse/basket/persistence/BasketDAO.java
@@ -7,5 +7,6 @@ import com.WhereHouse.basket.domain.BasketVO;
 public interface BasketDAO {
 	public void insertBasket(BasketVO vo)throws Exception;
 	public void deleteBasket(int b_no)throws Exception;
-	public List<BasketVO> listBasket()throws Exception;
+	public List<BasketVO> listBasketById(String m_id)throws Exception;
+	public List<BasketVO> listAllBasket()throws Exception;
 }
--- WhereHouse/src/main/java/com/WhereHouse/mypage/controller/MyPageController.java
+++ WhereHouse/src/main/java/com/WhereHouse/mypage/controller/MyPageController.java
@@ -1,32 +1,33 @@
 package com.WhereHouse.mypage.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.WhereHouse.basket.domain.BasketVO;
 import com.WhereHouse.basket.service.BasketService;
 
 @Controller
 @RequestMapping("/mypage")
 public class MyPageController {
+	
 	@Inject
 	private BasketService service;
+
 	
 	@RequestMapping("")
     public String mypage(Model model, HttpSession session) throws Exception{
-		
-		
-		
-		
 		return "/mypage/mypage";
     }
 	@RequestMapping("/basket")
-    public String listBasket(Model model, HttpSession session) throws Exception{
-		model.addAttribute("list", service.listAllById(session.getId()
-				));
+    public String listBasket(Model model, HttpServletRequest request) throws Exception{
+		String m_id = (String) request.getSession().getAttribute("m_id");
+		System.out.println("Session üũ : "+m_id);
+		model.addAttribute("list",service.listAllById(m_id));
 		return "/mypage/mypage";
     }
 
