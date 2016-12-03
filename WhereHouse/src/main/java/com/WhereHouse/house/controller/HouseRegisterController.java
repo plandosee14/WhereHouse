b77d850a081d737ca3a60174b9da91a104be7236
윤영미 <@192.168.0.116>
2016-12-03 09:36:28 +0900

    dd

--- WhereHouse/src/main/java/com/WhereHouse/house/controller/HouseRegisterController.java
+++ WhereHouse/src/main/java/com/WhereHouse/house/controller/HouseRegisterController.java
@@ -1,11 +1,21 @@
 package com.WhereHouse.house.controller;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.WhereHouse.house.domain.HouseVO;
+import com.WhereHouse.house.service.HouseService;
 
 @RequestMapping("/house")
 @Controller
 public class HouseRegisterController {
+	
+	@Inject
+	HouseService hservice;
 
 	@RequestMapping("registerForm")
 	public void RegisterForm(){
@@ -16,9 +26,18 @@ public class HouseRegisterController {
 		
 	}
 	
-	@RequestMapping("register")
-	public void Register(){
+	@RequestMapping("/register")
+	public void Register(HttpSession session, HouseVO house, MultipartFile file){
+		System.out.println("등록폼 데이터: "+house);
+		try {
+			hservice.insertHouse(house);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
+	
+
 }
