89eed000496100cee75c77bd780ac8d8f2a069d2
june <june@192.168.0.125>
2016-12-06 18:53:03 +0900

    commit

--- WhereHouse/src/main/java/com/WhereHouse/memberGrade/controller/GradeController.java
+++ WhereHouse/src/main/java/com/WhereHouse/memberGrade/controller/GradeController.java
@@ -1,12 +1,17 @@
 package com.WhereHouse.memberGrade.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
 import javax.inject.Inject;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.WhereHouse.memberGrade.domain.GradeVO;
 import com.WhereHouse.memberGrade.service.GradeService;
 
 @RestController
@@ -16,5 +21,8 @@ public class GradeController {
 	@Inject
 	private GradeService service;
 
-	
+	@RequestMapping(value="/{h_no}", method={RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<String> update(@PathVariable("h_no") int h_no, @RequestBody GradeVO vo) {
+		return null;
+	}
 }
